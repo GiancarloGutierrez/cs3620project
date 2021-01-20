84bd08b56ce41dfd6e50616fab8b8ca7c03ab077
Giancarlo Gutierrez <ggutierrez1@mail.weber.edu>
2021-01-19 18:52:19 -0700

    changed file name back

---
+++ books.php
@@ -0,0 +1,5 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+?>
+
+{"books-read":[{"Title":"SomeName1","Author":"Don't Know"},{"Title":"SomeName2", "Author":"Don't Know"}]}
--- test.php
+++
@@ -1,5 +0,0 @@
-<?php
-header("Access-conrol-allow-Origin: *");
-?>
-
-{"books-read":[{"Title":"The Hobbit","Author":"J. R. R. Tolkien"},{"Title":"The Body", "Author":"Bill Bryson"}]}
