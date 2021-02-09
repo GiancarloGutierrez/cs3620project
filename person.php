745e8f29d8a4422914ce693c53dc202e8dff3803
Giancarlo Gutierrez <ggutierrez1@mail.weber.edu>
2021-02-09 14:44:17 -0700

    Initial start of a new file structure for website user

---
+++ person.php
@@ -0,0 +1,13 @@
+<?php
+
+Class person{
+    public function getUser($user_id){
+
+    }
+
+    public function getUserName(){
+        return "Giancarlo";
+    }
+}
+
+?>
---
+++ user/userDAO.php
@@ -0,0 +1,8 @@
+<?php
+
+class userDAO{
+    public function getUser($user){
+        $user->setUsername("Giancarlo");
+    }
+}
+?>
